a1b0ecd8f1dad3a238538a463b0d3416c43e2197
Anton Fenske <anton.fenske@klarna.com>
2018-06-25 22:47:52 +0200

    Support new data model

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,29 +3,51 @@ import * as admin from 'firebase-admin'
 admin.initializeApp()
 
 export const updateCampaignProgress = functions.https.onRequest((request, response) => {
-    if (request.body.event == "Geofence Entered") {
+    const campaignId = "campaign-id-1"
+    if (request.body.event === "Geofence Entered") {
         const db = admin.firestore()
         const userRef = db.doc("/users/" + request.body.userId)
-        db.runTransaction(function(transaction) {
-            return transaction.get(userRef).then(function(user) {
-                console.info(user.data())
-                const updatedPlaces = user.data().places
-                for (const p in updatedPlaces) {
-                    const updatedCampaigns = updatedPlaces[p].campaigns;
-                    for (const c in updatedCampaigns) {
-                        updatedCampaigns[c] = updatedCampaigns[c] + 1;    
+        if (userRef != null) {
+            db.runTransaction(function(transaction) {
+                return transaction.get(userRef).then(function(user) {
+                    console.info("User snapshot: " + JSON.stringify(user.data()))
+                    const updatedCampaigns = user.data().campaigns
+                    if (updatedCampaigns[campaignId] != null) {
+                        const places = updatedCampaigns[campaignId].places
+                        if (places != null) {
+                            console.info("User: " + user.id + ". Progress before update: " + JSON.stringify(places))
+                            const enteredGeofenceId = request.body.properties.geofence_id
+                            if (enteredGeofenceId != null) {
+                                console.info("Entered Geofence: " + enteredGeofenceId)
+                                for (const p in places) {
+                                    if (p === enteredGeofenceId) {
+                                        updatedCampaigns[campaignId].places[p].visited = places[p].visited + 1
+                                        console.info("User: " + user.id + ". Progress after update: " + JSON.stringify(places))
+                                        transaction.update(userRef, {campaigns : updatedCampaigns})
+                                    }
+                                }
+                            } else {
+                                console.warn("No geofence id found in Radar request: " + JSON.stringify(request.body))
+                            }
+                        } else {
+                            console.warn("No defined places inside campaign " + campaignId + " for user " + user.id)    
+                        }
+                    } else { 
+                        console.warn("Predefined campaign " + campaignId + " not found for user " + user.id)
                     }
-                }
-                transaction.update(userRef, {places : updatedPlaces})
-                return updatedPlaces
+                    return updatedCampaigns
+                })
+            }).then(function(updatedCampaigns) {
+                return response.status(200).send()
+            }).catch(function(err) {
+                console.error(err)
+                return response.status(500).send()
             })
-        }).then(function(updatedPlaces) {
-            console.log("Updated places with ", updatedPlaces)
-            return response.status(200).send()
-        }).catch(function(err) {
-            console.error(err)
-            return response.status(500).send()
-        })
+        } else {
+            console.error("User " + request.body.userId + " not found.")
+        }
+    } else {
+        console.info("Event with type " + request.body.event + " not supported")
     }
     return response.status(200).send()
 });
